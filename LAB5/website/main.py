7952199015fd7b72035b6e357a523e2ba3edf988
Mafouz DIALLO <mafouzdiallo@gmail.com>
2022-06-03 22:14:35 +0000

    Add and config docker compose for connect app to redis

--- LAB5/website/main.py
+++ LAB5/website/main.py
@@ -5,11 +5,11 @@ from student import Student
 
 app = Flask(__name__, template_folder='templates')
 
-# conn = redis.Redis(host="redis", port=6379, db=0)
-# try:
-#     conn.ping()
-# except redis.exceptions.ConnectionError as err:
-#     raise err
+conn = redis.Redis(host="redis", port=6379, db=0)
+try:
+    conn.ping()
+except redis.exceptions.ConnectionError as err:
+    raise err
 
 
 @app.route("/", methods=["GET"])
@@ -17,15 +17,15 @@ def index():
     return render_template("index.html")
 
 
-# @app.route("/handle_data", methods=["POST"])
-# def handle_data():
-#     stud = Student(request, conn)
-#     stud.get_values()
-#     err = stud.record_student()
-#     if err:
-#         return render_template("error.html")
-#     results = stud.get_all_students()
-#     return render_template("result.html", results=results)
+@app.route("/handle_data", methods=["POST"])
+def handle_data():
+    stud = Student(request, conn)
+    stud.get_values()
+    err = stud.record_student()
+    if err:
+        return render_template("error.html")
+    results = stud.get_all_students()
+    return render_template("result.html", results=results)
 
 
 if __name__ == "__main__":
